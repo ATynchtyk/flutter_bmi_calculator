23a998a4ec8aa5564c444b6100f739897cae34bc
ATynchtyk <98882256+ATynchtyk@users.noreply.github.com>
2022-05-20 00:48:47 +0600

    ozgordu

--- lib/input_screen.dart
+++ lib/input_screen.dart
@@ -5,6 +5,12 @@ import 'package:flutter_bmi_calculator/widgets/slaider_widget.dart';
 import 'package:flutter_bmi_calculator/widgets/weight_age.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+enum Gender {
+  Male,
+  Female,
+  Non,
+}
+
 class InputScreen extends StatefulWidget {
   const InputScreen({Key key}) : super(key: key);
 
@@ -13,7 +19,14 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
-  double _slaider = 65;
+  double _slaider = 180;
+  int _weight = 60;
+  int _age = 25;
+  Color _activeColor = const Color.fromARGB(255, 76, 78, 100);
+  Color _inActiveColor = const Color(0xff111327);
+  bool _maleButton = false;
+  bool _femaleButton = false;
+  Gender _gender = Gender.Non;
 
   @override
   Widget build(BuildContext context) {
@@ -26,40 +39,77 @@ class _InputScreenState extends State<InputScreen> {
         children: [
           Expanded(
             child: Row(
-              children: const [
+              children: [
                 CustomWidget(
-                  widget:
-                      GenderWidget(icons: FontAwesomeIcons.male, text: 'male'),
+                  onTap: (() {
+                    setState(() {
+                      // _maleButton = !_maleButton;
+                      _gender = Gender.Male;
+                    });
+                  }),
+                  // tus: _maleButton ? _activeColor : _inActiveColor,
+                  tus: _gender == Gender.Male ? _activeColor : _inActiveColor,
+                  widget: const GenderWidget(
+                      icons: FontAwesomeIcons.person, text: 'male'),
                 ),
                 CustomWidget(
-                  widget: GenderWidget(
-                      icons: FontAwesomeIcons.female, text: 'female'),
+                  onTap: (() {
+                    setState(() {
+                      // _femaleButton = !_femaleButton;
+                      _gender = Gender.Female;
+                    });
+                  }),
+                  // tus: _femaleButton ? _activeColor : _inActiveColor,
+                  tus: _gender == Gender.Female ? _activeColor : _inActiveColor,
+                  widget: const GenderWidget(
+                      icons: FontAwesomeIcons.personDress, text: 'female'),
                 ),
               ],
             ),
           ),
-          CustomWidget(
-              widget: SlaiderWidget(
+          SlaiderWidget(
             onPressed: (double ozgorgonData) {
               setState(() {
                 _slaider = ozgorgonData;
               });
             },
             slaiderText: _slaider.toInt(),
-          )),
+          ),
           Expanded(
             child: Row(
-              children: const [
+              children: [
                 CustomWidget(
+                  tus: const Color(0xff111327),
                   widget: WeightAgeWidget(
+                    minus: () {
+                      setState(() {
+                        _weight--;
+                      });
+                    },
+                    plus: () {
+                      setState(() {
+                        _weight++;
+                      });
+                    },
                     text: 'weight',
-                    texttwo: '60',
+                    texttwo: _weight.toString(),
                   ),
                 ),
                 CustomWidget(
+                  tus: const Color(0xff111327),
                   widget: WeightAgeWidget(
+                    minus: () {
+                      setState(() {
+                        _age--;
+                      });
+                    },
+                    plus: () {
+                      setState(() {
+                        _age++;
+                      });
+                    },
                     text: 'age',
-                    texttwo: '25',
+                    texttwo: _age.toString(),
                   ),
                 ),
               ],
